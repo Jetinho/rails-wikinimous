3b2b4aa8f0c1d8bf7ecc26157aeb130a13f572c4
Julien Jet <jet.julien@gmail.com>
2016-11-22 15:38:25 -0200

    Implement too many things : edit update destroy and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'articles', to: 'articles#index'
+  # get 'articles/new', to: 'articles#new'
+  # get 'articles/:id', to: 'articles#show'
+  # get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  # post 'articles/create', to: 'articles#create'
+  # patch 'articles/:id', to: 'articles#update'
+  # delete 'articles/:id', to: 'articles#destroy'
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
